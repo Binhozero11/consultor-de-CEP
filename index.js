f067c9d337d06febdc894dd5a3dc115526b9ed0e
Binhozero11 <fabiodejesusxavier64@gmail.com>
2023-11-14 15:15:01 -0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const form = document.getElementById("form")
 const res = document.getElementById("resultado")
 const button= document.querySelector("button[type='submit']")
+const historico = document.getElementById("historico")
+
+document.addEventListener('novo_cep_consultado', function(){
+    meusUltimosCepConsultados();
+})
+
+meusUltimosCepConsultados()
 
 async function consultarCep(cep) {
     //
@@ -36,34 +43,61 @@ async function consultarCep(cep) {
             ibge,
             ddd,
         }
-
-       
     }
   
-
     }catch(e){
         throw new Error(`Algo deu errado, tente novamente, error: ${e.message}`)
     }
 }
-//     .then(resultado => {
-//         if (resultado.status >= 400) {
-//             return Promise.reject()    
-//         }
-//         return resultado.json()
-//     })
-//     .then(json => {
 
-// const {cep, logradouro, complemento , bairro, localidade, uf, ibge, ddd } = json;
+function notificarCepConsultado(cep) {
+    const event = new CustomEvent("novo_cep_consultado", { detail:  cep});
+    document.dispatchEvent(event);
+}
 
-//         res.innerHTML = localidade
-//     })
-//     .catch(err => console.log(err))
-//     .finally(function(){
-//         res.classList.remove("spinner-border")
-//     })
-// }
+function salvarCepConsultado(cep){
+    let ceps = cepsStorages()
+    ceps.unshift(cep)
+    localStorage.setItem("ceps", JSON.stringify(ceps))
+    notificarCepConsultado(cep)
+}
+
+function cepsStorages() {
+    let ceps = localStorage.getItem("ceps")
+   
+    if(ceps!=null){
+        return JSON.parse(ceps)
+    }
+
+    localStorage.setItem("ceps", JSON.stringify([]))
+    return [];
+}
+
+function meusUltimosCepConsultados() {
+    let ceps = cepsStorages()
+   
+  
+    historico.innerHTML = ""
+    var index =0;
+    for (const cep of ceps) {
+        
+        if(index>=5){
+            break;
+        }
+        historico.innerHTML += `<p>${cep.localidade} ${cep.cep}</p>`
+        index++
+    }
+       
+    
+}
+
 function errorMessage(msg) {
-    res.innerHTML= `<span class="message-error">${msg}</span>`
+    res.innerHTML= 
+    `
+        <span>
+            <div class="message-error">${msg}</div>
+        </span>
+    `
 }
 
 function okMessage(msg) {
@@ -83,26 +117,59 @@ form.addEventListener("submit", async function(e) {
     const cepDigitado = e.target.cep.value;
     const validaCep = /^[0-9]{8}$/
 
-
-
-
     const isValido = validaCep.test(cepDigitado)
     res.innerHTML = ''
     if (!isValido) {
         errorMessage("CEP inválido")
         return;
     }
+
+    //existo do local sgora, se existe reroan ele
+
+  
    
     const value = cepDigitado.replace(/\D/g, '');
+
+    const ceps = cepsStorages()
+    const valor = ceps.filter(cep=> cep.cep.replace(/\D/g, '') == value)
+   if(valor.length>0){
+   const resultado = valor[0]
+
+   const uri = new URLSearchParams();
+   uri.append("query", `${resultado.logradouro},${resultado.localidade}` )
+  
+
+
+   const url = `https://www.google.com/maps/search/?api=1&${uri.toString()}`
+
+   okMessage(
+    `
+        <li>Local: <strong>${resultado.localidade.length > 0 ? resultado.localidade : "..."}</strong></li>
+        <li>DDD: <strong>${resultado.ddd.length > 0 ? resultado.ddd : "..."}</strong></li>
+        <li>Bairro: <strong>${resultado.bairro.length > 0 ? resultado.bairro : "..."}</strong></li>
+        <li>Ibge: <strong>${resultado.ibge.length > 0 ? resultado.ibge : "..."}</strong></li>
+        <li>Complemento: <strong>${resultado.complemento.length > 0 ? resultado.complemento : "..."}</strong></li>
+        <li>Logradouro: <strong>${resultado.logradouro.length > 0 ? resultado.logradouro : "..."}</strong></li>
+        <li>Uf: <strong>${resultado.uf.length > 0 ? resultado.uf : "..."}</strong></li>
+        ${resultado.localidade.length>0 ? `    <li>
+        <a target="_blank" href="${url}">Veja no mapa</a>
+    </li>` : "" }`)
+    return;
+   }
+
 
 
     res.classList.add("spinner-border")
     const resultado = await consultarCep(value)
-    console.log(resultado);
+
     if(resultado==undefined) {
         errorMessage(`Não conseguimos encontrar nenhuma informação com CEP ${cepDigitado} informado`)
         return
     }
+    console.log('salvarCepConsultado', resultado)
+
+    salvarCepConsultado(resultado)
+
     const uri = new URLSearchParams();
     uri.append("query", `${resultado.logradouro},${resultado.localidade}` )
    
