d93aebce896b2dadc03a4221f394ece473b28c4a
Binhozero11 <fabiodejesusxavier64@gmail.com>
2023-11-14 16:19:29 -0300

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const form = document.getElementById("form")
 const res = document.getElementById("resultado")
-const button= document.querySelector("button[type='submit']")
+const button = document.querySelector("button[type='submit']")
 const historico = document.getElementById("historico")
 
-document.addEventListener('novo_cep_consultado', function(){
+document.addEventListener('novo_cep_consultado', function () {
     meusUltimosCepConsultados();
 })
 
@@ -13,49 +13,49 @@ async function consultarCep(cep) {
     //
     try {
         const resultado = await fetch(`https://viacep.com.br/ws/${cep}/json/`, {
-        method: "GET",
-    })
+            method: "GET",
+        })
 
-    if (resultado.ok) {
-        const body = await resultado.json()
+        if (resultado.ok) {
+            const body = await resultado.json()
 
-        if(body ===undefined)
-           return undefined
+            if (body === undefined)
+                return undefined
 
-        const {cep, logradouro, complemento , bairro, localidade, uf, ibge, ddd, erro }
-         = body
+            const { cep, logradouro, complemento, bairro, localidade, uf, ibge, ddd, erro }
+                = body
 
-        if(erro) {
-            return undefined  
+            if (erro) {
+                return undefined
+            }
+
+            if (cep === undefined) {
+                return undefined
+            }
+
+            return {
+                cep,
+                logradouro,
+                uf,
+                complemento,
+                bairro,
+                localidade,
+                ibge,
+                ddd,
+            }
         }
 
-         if(cep===undefined){
-            return undefined 
-        }
-
-        return {
-            cep,
-            logradouro,
-            uf,
-            complemento,
-            bairro,
-            localidade,
-            ibge,
-            ddd,
-        }
-    }
-  
-    }catch(e){
+    } catch (e) {
         throw new Error(`Algo deu errado, tente novamente, error: ${e.message}`)
     }
 }
 
 function notificarCepConsultado(cep) {
-    const event = new CustomEvent("novo_cep_consultado", { detail:  cep});
+    const event = new CustomEvent("novo_cep_consultado", { detail: cep });
     document.dispatchEvent(event);
 }
 
-function salvarCepConsultado(cep){
+function salvarCepConsultado(cep) {
     let ceps = cepsStorages()
     ceps.unshift(cep)
     localStorage.setItem("ceps", JSON.stringify(ceps))
@@ -64,8 +64,8 @@ function salvarCepConsultado(cep){
 
 function cepsStorages() {
     let ceps = localStorage.getItem("ceps")
-   
-    if(ceps!=null){
+
+    if (ceps != null) {
         return JSON.parse(ceps)
     }
 
@@ -75,130 +75,141 @@ function cepsStorages() {
 
 function meusUltimosCepConsultados() {
     let ceps = cepsStorages()
-   
-  
     historico.innerHTML = ""
-    var index =0;
+    var index = 0;
     for (const cep of ceps) {
-        
-        if(index>=5){
+        if (index >= 5) {
             break;
         }
-        historico.innerHTML += `<p>${cep.localidade} ${cep.cep}</p>`
+        const p = document.createElement('p')
+        p.innerHTML = `${cep.localidade} ${cep.cep}`
+        p.addEventListener('click', function () {
+            tratadorCep(cep)
+        })
+
+        historico.appendChild(p)
         index++
     }
-       
-    
+
+
 }
 
 function errorMessage(msg) {
-    res.innerHTML= 
-    `
+    res.innerHTML =
+        `
         <span>
             <div class="message-error">${msg}</div>
         </span>
     `
 }
 
+function tratadorCep(resultado){
+    const uri = new URLSearchParams();
+    uri.append("query", `${resultado.logradouro},${resultado.localidade}`)
+    const url = `https://www.google.com/maps/search/?api=1&${uri.toString()}`
+
+    okMessage(
+        `
+    <li>Local: <strong>${resultado.localidade.length > 0 ? resultado.localidade : "..."}</strong></li>
+    <li>DDD: <strong>${resultado.ddd.length > 0 ? resultado.ddd : "..."}</strong></li>
+    <li>Bairro: <strong>${resultado.bairro.length > 0 ? resultado.bairro : "..."}</strong></li>
+    <li>Ibge: <strong>${resultado.ibge.length > 0 ? resultado.ibge : "..."}</strong></li>
+    <li>Complemento: <strong>${resultado.complemento.length > 0 ? resultado.complemento : "..."}</strong></li>
+    <li>Logradouro: <strong>${resultado.logradouro.length > 0 ? resultado.logradouro : "..."}</strong></li>
+    <li>Uf: <strong>${resultado.uf.length > 0 ? resultado.uf : "..."}</strong></li>
+    ${resultado.localidade.length > 0 ? `    <li>
+    <a target="_blank" href="${url}">Veja no mapa</a>
+</li>` : ""}
+
+`
+    )
+
+}
+
 function okMessage(msg) {
-    res.innerHTML= 
-    `
+    res.innerHTML =
+        `
         <span>
             <ul class="message-ok">${msg}</ul>
         </span>
     `
 }
 
-form.addEventListener("submit", async function(e) {
+function consultaCepNoStorage(valor){
+    const ceps = cepsStorages()
+    const ultimosCepConsultado = ceps.filter(cep => cep.cep.replace(/\D/g, '') == valor)
+    if (ultimosCepConsultado.length > 0) {
+        const resultado = ultimosCepConsultado[0]
+       
+        return resultado;
+    }
+
+    return undefined
+} 
+
+function cepValido(cep){
+    const validaCep = /^[0-9]{8}$/
+    const isValido = validaCep.test(cep)
+    return isValido
+}
+
+function removeCaracteresCep(cep) {
+    const value = cep.replace(/\D/g, '');
+    return value
+}
+
+function antesConsultarCep() {
+    res.innerHTML = ''
+    button.disabled = true;
+    res.classList.add("spinner-border")
+}
+
+function aposcoNSULTAdOcEP() {
+    res.classList.remove("spinner-border")
+        button.disabled = false;
+}
+
+form.addEventListener("submit", async function (e) {
 
     try {
-    e.preventDefault()
-    button.disabled = true;
-    const cepDigitado = e.target.cep.value;
-    const validaCep = /^[0-9]{8}$/
+        e.preventDefault()
+        antesConsultarCep();
 
-    const isValido = validaCep.test(cepDigitado)
-    res.innerHTML = ''
-    if (!isValido) {
-        errorMessage("CEP inválido")
-        return;
-    }
+        const cepDigitado = e.target.cep.value;
+        const isValido= cepValido(cepDigitado)
 
-    //existo do local sgora, se existe reroan ele
-
-  
-   
-    const value = cepDigitado.replace(/\D/g, '');
-
-    const ceps = cepsStorages()
-    const valor = ceps.filter(cep=> cep.cep.replace(/\D/g, '') == value)
-   if(valor.length>0){
-   const resultado = valor[0]
-
-   const uri = new URLSearchParams();
-   uri.append("query", `${resultado.logradouro},${resultado.localidade}` )
-  
-
-
-   const url = `https://www.google.com/maps/search/?api=1&${uri.toString()}`
-
-   okMessage(
-    `
-        <li>Local: <strong>${resultado.localidade.length > 0 ? resultado.localidade : "..."}</strong></li>
-        <li>DDD: <strong>${resultado.ddd.length > 0 ? resultado.ddd : "..."}</strong></li>
-        <li>Bairro: <strong>${resultado.bairro.length > 0 ? resultado.bairro : "..."}</strong></li>
-        <li>Ibge: <strong>${resultado.ibge.length > 0 ? resultado.ibge : "..."}</strong></li>
-        <li>Complemento: <strong>${resultado.complemento.length > 0 ? resultado.complemento : "..."}</strong></li>
-        <li>Logradouro: <strong>${resultado.logradouro.length > 0 ? resultado.logradouro : "..."}</strong></li>
-        <li>Uf: <strong>${resultado.uf.length > 0 ? resultado.uf : "..."}</strong></li>
-        ${resultado.localidade.length>0 ? `    <li>
-        <a target="_blank" href="${url}">Veja no mapa</a>
-    </li>` : "" }`)
-    return;
-   }
-
-
-
-    res.classList.add("spinner-border")
-    const resultado = await consultarCep(value)
-
-    if(resultado==undefined) {
-        errorMessage(`Não conseguimos encontrar nenhuma informação com CEP ${cepDigitado} informado`)
-        return
-    }
-    console.log('salvarCepConsultado', resultado)
-
-    salvarCepConsultado(resultado)
-
-    const uri = new URLSearchParams();
-    uri.append("query", `${resultado.logradouro},${resultado.localidade}` )
-   
-
-
-    const url = `https://www.google.com/maps/search/?api=1&${uri.toString()}`
-
-    okMessage(
-    `
-        <li>Local: <strong>${resultado.localidade.length > 0 ? resultado.localidade : "..."}</strong></li>
-        <li>DDD: <strong>${resultado.ddd.length > 0 ? resultado.ddd : "..."}</strong></li>
-        <li>Bairro: <strong>${resultado.bairro.length > 0 ? resultado.bairro : "..."}</strong></li>
-        <li>Ibge: <strong>${resultado.ibge.length > 0 ? resultado.ibge : "..."}</strong></li>
-        <li>Complemento: <strong>${resultado.complemento.length > 0 ? resultado.complemento : "..."}</strong></li>
-        <li>Logradouro: <strong>${resultado.logradouro.length > 0 ? resultado.logradouro : "..."}</strong></li>
-        <li>Uf: <strong>${resultado.uf.length > 0 ? resultado.uf : "..."}</strong></li>
-        ${resultado.localidade.length>0 ? `    <li>
-        <a target="_blank" href="${url}">Veja no mapa</a>
-    </li>` : "" }
     
-    `
-    )
+        if (!isValido) {
+            errorMessage("CEP inválido")
+            return;
+        }
 
+        const value = removeCaracteresCep(cepDigitado);       
 
-    }catch(e){
+        const ultimoCepConsultado=  consultaCepNoStorage(value)
         
-    }finally{
-        res.classList.remove("spinner-border")
-        button.disabled = false;
+        if (ultimoCepConsultado!=undefined) {
+           
+            salvarCepConsultado(ultimoCepConsultado)
+            return;
+        }
+     
+        const resultado = await consultarCep(value)
 
+        if (resultado == undefined) {
+            errorMessage(`Não conseguimos encontrar nenhuma informação com o CEP ${cepDigitado} informado`)
+            return
+        }
+
+        salvarCepConsultado(resultado)
+
+        tratadorCep(resultado)
+
+
+    } catch (e) {
+
+    } finally {
+        
+        aposcoNSULTAdOcEP()
     }
 })
